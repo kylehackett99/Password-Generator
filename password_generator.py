436d3daeddccd95c63a4151a46fc2dabad28ab34
kylehackett99 <kylejhackett99@gmail.com>
2020-06-06 09:37:02 -0400

    m

--- password_generator.py
+++ password_generator.py
@@ -43,7 +43,6 @@ def char_generator(size, chars=string.ascii_uppercase + string.digits + string.a
     return ''.join(random.choice(chars) for _ in range(size))
 
 def gen_pass(input,psswrd,length,rw):
-    length=10
     new_string = ''
     input =  input.replace(" ", "")
     replacements = (('a','4'), ('e','3'),('i','!'), ('o','0'), ('t','+'),('u',"U"),('l','1'))
@@ -57,14 +56,13 @@ def gen_pass(input,psswrd,length,rw):
     if(len(input) < length):
         temp = length - len(input)
         new_string= char_generator(temp) 
-        print(new_string)        
 
     new_string = input + new_string
 
     #Checks to see if it's been cracked before
     sha1pwd, count = lookup_pwned_api(new_string)
     if count:
-        rw.passlbl.config(text="Bad Password. Run Again.")
+        rw.passlbl.config(text="Bad Password. Run Again.\nTry Changing the Phrase.")
         psswrd.password = ''
         return 0
         
@@ -80,7 +78,7 @@ def copy_clicked(str):
     copy_to_clipboard(str)
 
 
-class ResizableWindow:
+class ResizeWindow:
     def __init__(self, parent,psswrd):
         self.parent = parent
         self.f1_style = ttk.Style()
@@ -88,24 +86,39 @@ class ResizableWindow:
         self.f1 = ttk.Frame(self.parent, style='My.TFrame', padding=(3, 3, 12, 12))  
         self.f1.grid(column=0, row=0, sticky=(N, S, E, W))  
         
-        self.namelbl = ttk.Label(self.f1, text="Phrase")
-        self.name = ttk.Entry(self.f1)
-        self.namelbl.grid(column=0, row=0, columnspan=2, sticky=(N,W), padx=5)  
-        self.name.grid(column=0, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
-
-        self.passlbl = Label(self.f1, height=2,width=30,justify=CENTER)
-        self.copy_btn = Button(self.f1, text="Copy to Clipboard",command=lambda: copy_clicked(psswrd.password))
-        self.passlbl.grid(column=4,row=1, padx=5, pady=5)
-        self.copy_btn.grid(column=2, row=35, padx=5, pady=5)
-
-        self.runbtn = ttk.Button(self.f1, text="Run",command=lambda: run_clicked(self,psswrd))
-        self.runbtn.grid(column=2, row=5, padx=5, pady=5, sticky=(N,E,W))   
-
-        self.sizelbl = ttk.Label(self.f1, text="Size")
-        self.size = ttk.Spinbox(self.f1,from_= 10, to= 100, width=19)
+        self.phraselbl = ttk.Label(self.f1, text="Phrase")
+        self.phrase = ttk.Entry(self.f1)
+        self.phraselbl.grid(column=0, row=0, columnspan=2, sticky=(N,W), padx=5)  
+        self.phrase.grid(column=0, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)  # added sticky, pady, padx
+       
+        self.phraselbl.config(font=("Georgia",22))
+        self.phrase.config(font=("Georgia",20))
+        self.sizelbl = Label(self.f1, text="Size")
+        self.size = Spinbox(self.f1,from_= 10, to= 100, width=19)
         self.sizelbl.grid(column=0,row=2,columnspan=2,sticky=(N,W),padx=5)
-        self.size.grid(column=0,row=3,columnspan=2,sticky=(N, E, W),pady=5, padx=5)
+        self.size.grid(column=0,row=3,columnspan=2,sticky=(N, E, W), padx=5)
+        self.sizelbl.config(font=("Georgia",22))
+        self.size.config(font=("Georgia",20))
 
+
+        self.pass_lbl = Label(self.f1, text=" Generated Passsword:")
+        self.pass_lbl.grid(column=5,row=1, padx=5, pady=5)
+        self.passlbl = Label(self.f1, text= '        ')
+        self.passlbl.grid(column=5,row=2, padx=5, pady=5)
+        self.passlbl.config(font=("Georgia",22))
+        self.pass_lbl.config(font=("Georgia",40))
+
+    
+
+        self.runbtn = Button(self.f1, text="Run",command=lambda: run_clicked(self,psswrd))
+        self.runbtn.grid(column=2, row=5, padx=5, pady=5, sticky=(N,E,W))
+        self.runbtn.config(font=("Georgia",20))
+
+        self.copy_btn = Button(self.f1, text="Copy to Clipboard",command=lambda: copy_clicked(psswrd.password))
+        self.copy_btn.grid(column=2, row=35, padx=5, pady=5, sticky=(N,E,W) )
+        self.copy_btn.config(font=("Georgia",20))
+
+        
         #Resizes configurations
         self.parent.columnconfigure(0, weight=1)
         self.parent.rowconfigure(0, weight=1)
@@ -120,7 +133,6 @@ class ResizableWindow:
 
 def run_clicked(rw,psswrd):
         psswrd.size = rw.size.get()
-        print(psswrd.size)
         
         #Convert length to int somehow 
         length = psswrd.size
@@ -129,7 +141,7 @@ def run_clicked(rw,psswrd):
         else:
             length = int(length)
         
-        state = gen_pass(rw.name.get(),psswrd,length,rw)
+        state = gen_pass(rw.phrase.get(),psswrd,length,rw)
         if(state == 1):
             rw.passlbl.config(text=psswrd.password)
 
@@ -138,7 +150,7 @@ def main(argv):
     root = Tk()
     root.title("Password Generator")
     psswrd = Password(" ", '10', " ")
-    rw = ResizableWindow(root,psswrd)
+    rw = ResizeWindow(root,psswrd)
     root.mainloop()
     
 if __name__ == "__main__":
